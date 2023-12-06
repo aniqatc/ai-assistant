51ffb493064423ec76072c2c9ad0daf854d12d5b
aniqatc <aniqatc97@gmail.com>
2023-12-06 02:22:00 -0500

    styles: heading, custom radio buttons, initial layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,16 @@ module.exports = {
 				mono: ['Source Code Pro', 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'monospace'],
 			},
 			colors: {},
+			container: {
+				center: true,
+				screens: {
+					sm: '640px',
+					md: '768px',
+					lg: '1024px',
+					xl: '1024px',
+					'2xl': '1024px',
+				},
+			},
 		},
 	},
 	plugins: [],
