fcccac0ea5bd5e929b96ad162ad5bd303d86e135
aniqatc <aniqatc97@gmail.com>
2023-12-12 21:00:15 -0500

    prettier format + add additional command to json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,52 @@
 module.exports = {
-	content: ['./src/**/*.html', './src/**/*.js'],
-	darkMode: 'class',
-	theme: {
-		screens: {
-			xs: '465px',
-			sm: '640px',
-			md: '768px',
-			lg: '1024px',
-		},
-		extend: {
-			fontFamily: {
-				sans: ['Noto Sans', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'sans-serif'],
-				mono: ['Source Code Pro', 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'monospace'],
-			},
-			colors: {},
-			container: {
-				center: true,
-				screens: {
-					sm: '640px',
-					md: '768px',
-					lg: '1024px',
-					xl: '1024px',
-					'2xl': '1024px',
-				},
-			},
-			keyframes: {
-				slideInY: {
-					'0%': { transform: 'translateY(-100%)', opacity: '0' },
-					'100%': { transform: 'translateY(0)', opacity: '1' },
-				},
-			},
-			animation: {
-				'slide-in': 'slideInY 500ms ease-out both',
-			},
-		},
-	},
-	plugins: [require('tailwindcss-animation-delay')],
+  content: ["./src/**/*.html", "./src/**/*.js"],
+  darkMode: "class",
+  theme: {
+    screens: {
+      xs: "465px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+    },
+    extend: {
+      fontFamily: {
+        sans: [
+          "Noto Sans",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "sans-serif",
+        ],
+        mono: [
+          "Source Code Pro",
+          "ui-monospace",
+          "SFMono-Regular",
+          "Menlo",
+          "Monaco",
+          "monospace",
+        ],
+      },
+      colors: {},
+      container: {
+        center: true,
+        screens: {
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1024px",
+          "2xl": "1024px",
+        },
+      },
+      keyframes: {
+        slideInY: {
+          "0%": { transform: "translateY(-100%)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+      },
+      animation: {
+        "slide-in": "slideInY 500ms ease-out both",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animation-delay")],
 };
